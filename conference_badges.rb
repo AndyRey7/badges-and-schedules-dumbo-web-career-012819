9180199edd8698b2abd393d168334f92ff3987cf
andy reyes <11710reyes@gmail.com>
2018-12-28 04:27:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,13 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
+def batch_badge_creator
+  messages = []
+end
+
+def assign_rooms
+  messages = []
+  room_assignments =[]
+end
+
+def printer
