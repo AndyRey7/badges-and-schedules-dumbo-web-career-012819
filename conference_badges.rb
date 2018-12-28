b6aa6863e834aaa755131d2dd4bc93d9ce9d6ac3
andy reyes <11710reyes@gmail.com>
2018-12-28 04:43:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,12 @@
-def badge_maker(n)
-  return "Hello, my name is #{n}."
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(msg)
    array = []
    msg.each do |name|
-     array.push()
+     array.push("Hello, my name is #{name}.")
+   end
 end
 
 def assign_rooms(assign)
